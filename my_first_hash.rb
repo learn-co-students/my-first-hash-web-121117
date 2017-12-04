9acbc3197f74846782686732d3cdac384859ca6b
github username <github email address>
2017-12-03 18:58:00 -0800

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,31 +1,19 @@
 def my_hash
-    your_hash = {"my_first_name" => "Drew", "my_last_name" => "Overcash"}
+  my_hash = {"name" => "Drew", "age" => "30", "dream" => "Good Question"}
 end
 
 
 def shipping_manifest
-    shipping_manifest = {
-    "whale bone corsets" => 5,
-    "porcelain vases" => 2,
-    "oil paintings" => 3
-    }
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
-
-
 
 def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  
   shipping_manifest["oil paintings"]
 end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  
   shipping_manifest["pearl necklace"] = 1
-  
-  return shipping_manifest
+  shipping_manifest
 end
-
-
-
